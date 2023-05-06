b2126c26c54508cac61b522a7c20dd855efd4182
diims.el <diims.el@gmail.com>
2023-05-06 20:31:19 +0700

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,17 @@ module.exports = {
         filename: './index.html',
         template: './src/template.html',
     }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  }
 };
